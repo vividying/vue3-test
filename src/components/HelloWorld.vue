6d3f59d2f59fa24a031ae2349c68a48d28e2b342
liuxiaoying <1210226155@qq.com>
2023-04-09 20:29:58 +0800

    再次修改

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,5 +1,5 @@
 <template>
-  <h1>{{ msg }}b分支</h1>
+  <h1>{{ msg }}b分支222222</h1>
   <button @click="count++">count is: {{ count }}</button>
   <p>
     Edit <code>components/HelloWorld.vue</code> to test hot module replacement.
